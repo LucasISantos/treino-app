46dba4d6af5b1d6d017e06e38e74d8b2d440f484
LucasISantos <82480217+LucasISantos@users.noreply.github.com>
2026-03-26 02:26:22 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener('fetch', e => {});
+const CACHE='controle-treinos-v21';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
